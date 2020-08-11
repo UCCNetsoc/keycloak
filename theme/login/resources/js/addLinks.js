ed3a91eb31bd4600320053611347c82469d492a7
Oisín Canty <git@ocanty.com>
2020-08-11 01:25:08 +0100

    update links

--- theme/login/resources/js/addLinks.js
+++ theme/login/resources/js/addLinks.js
@@ -2,9 +2,9 @@
 window.onload = () => {
   const form = document.getElementById("kc-form-buttons");
   form.insertAdjacentHTML('beforeend',`
-    <button type="button" style="background-color: #4CAF50" class="btn" onclick="window.open('http://admin.netsoc.co/accounts/sign-up');">Sign Up</button>
-    <button type="button" style="background-color: rgba(0,0,0,0.20)" class="btn" onclick="window.open('http://admin.netsoc.co/accounts/password-reset');">Reset Password</button>
-    <button type="button" style="background-color: rgba(0,0,0,0.20)" class="btn" onclick="window.open('http://admin.netsoc.co/accounts/verification-email');">Resend Verification</button>
+    <button type="button" style="background-color: #4CAF50" class="btn" onclick="window.open('https://admin.netsoc.co/accounts/sign-up');">Sign Up</button>
+    <button type="button" style="background-color: rgba(0,0,0,0.20)" class="btn" onclick="window.open('https://admin.netsoc.co/accounts/password-reset');">Reset Password</button>
+    <button type="button" style="background-color: rgba(0,0,0,0.20)" class="btn" onclick="window.open('https://admin.netsoc.co/accounts/verification-email');">Resend Verification</button>
     <div style="
       margin: 0 auto;
       text-align: center;
@@ -12,9 +12,9 @@ window.onload = () => {
       font-size: 11px;
       color: gray;
     ">
-      <a target="_blank" href="https://raw.githubusercontent.com/UCCNetsoc/privacy-policy/master/README.md">Acceptable Usage Policy</a>
+      <a target="_blank" href="https://wiki.netsoc.co/en/services/acceptable-usage-policy">Acceptable Usage Policy</a>
       <span> &bull; </span>
-      <a target="_blank" href="https://raw.githubusercontent.com/UCCNetsoc/acceptable-usage-policy/master/README.md">Privacy Policy</a>
+      <a target="_blank" href="https://wiki.netsoc.co/en/services/privacy-policy">Privacy Policy</a>
       <p>Your use of our services is in agreement of our Acceptable Usage &amp; Privacy Policies</p>
     </div>
   `);
@@ -28,7 +28,8 @@ window.onload = () => {
     color: gray;
     margin-bottom: 16px;
   ">
-    <p>If you had signed up to a previous version of netsocadmin, you will need to reset your password</p>
+    <p>if you had signed up to netsocadmin prior to the 2020/2021 academic year, you will need to <a target="_blank" href="https://admin.netsoc.co/account/password-reset">reset your password</a></p>
   </div>`
 }
+
 
