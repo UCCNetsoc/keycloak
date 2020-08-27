46930dae7e4c15442ddf5ed98eb0f5cc2826b0c5
Oisín Canty <git@ocanty.com>
2020-08-27 09:21:53 +0100

    updated to use tos msg + fixed links

--- theme/login/resources/js/addLinks.js
+++ theme/login/resources/js/addLinks.js
@@ -12,10 +12,9 @@ window.onload = () => {
       font-size: 11px;
       color: gray;
     ">
-      <a target="_blank" href="https://wiki.netsoc.co/en/services/acceptable-usage-policy">Acceptable Usage Policy</a>
+      <a target="_blank" href="https://wiki.netsoc.co/en/services/terms-of-service">Terms of Service</a>
       <span> &bull; </span>
       <a target="_blank" href="https://wiki.netsoc.co/en/services/privacy-policy">Privacy Policy</a>
-      <p>Your use of our services is in agreement of our Acceptable Usage &amp; Privacy Policies</p>
     </div>
   `);
 
@@ -28,7 +27,7 @@ window.onload = () => {
     color: gray;
     margin-bottom: 16px;
   ">
-    <p>if you had signed up to netsocadmin prior to the 2020/2021 academic year, you will need to <a target="_blank" href="https://admin.netsoc.co/account/password-reset">reset your password</a></p>
+    <p>if you had signed up to netsocadmin prior to the 2020/2021 academic year, you will need to <a target="_blank" href="https://admin.netsoc.co/accounts/password-reset">reset your password</a></p>
   </div>`
 }
 
